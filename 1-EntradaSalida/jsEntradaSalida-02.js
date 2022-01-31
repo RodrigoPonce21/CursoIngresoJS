a407d63a58f6bc8aff11ce769f37fa9ecd087ae1
Rodrigo Ponce <rodrigooponcee2019@gmail.com>
2022-01-31 11:22:03 -0300

    CLASE 1 EJERCICIO 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
+//Rodrigo Ponce DIV D E/S 02
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombreDelAlumno;
+	nombreDelAlumno=prompt ("Ingrese su Nombre!!");
+	
+	alert(nombreDelAlumno)
 }
 
