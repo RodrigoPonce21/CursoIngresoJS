f6b766c8d267526597abe91d440c7773f0ba11da
Rodrigo Ponce <rodrigooponcee2019@gmail.com>
2022-01-31 10:30:05 -0300

    clase 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
+/*Ponce Rodrigo DIV D 
+	E/S 01			 */
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
 	alert("Hola Mundo");
+	alert("Hola Profe")
 }
 
